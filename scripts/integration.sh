6df39035c7ce09dbaa85a37a7bbf001d0d6d9287
Jaume Marhuenda <jaumemarhuenda@gmail.com>
2018-08-26 21:18:49 -0700

    tag integration tests

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -95,8 +95,8 @@ function run_tests() {
     export TRANSPORT="binary"
     export AUTH="KERBEROS"
     export SSL="0"
-    go test -race -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
-    go test -covermode=count -coverprofile=a.part -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
+    go test -tags integration -race -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
+    go test -tags integration -covermode=count -coverprofile=a.part -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
 
     # Tests with http transport and kerberos authentication
     setHttpTransport
@@ -107,8 +107,8 @@ function run_tests() {
     export TRANSPORT="http"
     export AUTH="KERBEROS"
     export SSL="1"
-    go test -race -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
-    go test -covermode=count -coverprofile=b.part -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
+    go test -tags integration -race -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
+    go test -tags integration -covermode=count -coverprofile=b.part -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
 
     # Tests with binary transport and none authentication
     setHive config/hive.cfg
@@ -117,8 +117,8 @@ function run_tests() {
     export TRANSPORT="binary"
     export AUTH="NONE"
     export SSL="0"
-    go test -race -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
-    go test -covermode=count -coverprofile=c.part -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
+    go test -tags integration -race -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
+    go test -tags integration -covermode=count -coverprofile=c.part -v -run . || { echo "Failed TRANSPORT=$TRANSPORT, AUTH=$AUTH, SSL=$SSL" ; docker logs hs2.example ; exit 2; }
 
     echo "mode: count" >coverage.out
     grep -h -v "mode: count" *.part >>coverage.out
