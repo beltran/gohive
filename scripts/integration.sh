0ad3ac31294ba410aaa0d8319a189b62c823e295
beltran <jaumemarhuenda@gmail.com>
2019-08-31 21:13:52 -0700

    change hive test version to 3.1.2

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -27,7 +27,7 @@ function wait_for_hive () {
 function install_deps() {
     if [ ! -d "dhive" ] ; then
         git clone https://github.com/beltran/dhive
-    else 
+    else
         cd dhive
         git fetch && git reset --hard origin/master
         cd ..
@@ -37,8 +37,8 @@ function install_deps() {
     sed -i.bak 's/python3/python3.6/g' ./Makefile
 
     python3.6 -m pip install --user -r requirements.txt
-    sed -i.bak 's/hive_version.*/hive_version = 3.1.1/g' ./config/hive.cfg
-    sed -i.bak 's/hive_version.*/hive_version = 3.1.1/g' ./config/hive_and_kerberos.cfg
+    sed -i.bak 's/hive_version.*/hive_version = 3.1.2/g' ./config/hive.cfg
+    sed -i.bak 's/hive_version.*/hive_version = 3.1.2/g' ./config/hive_and_kerberos.cfg
     sed -i.bak 's/hive.server2.thrift.sasl.qop.*/hive.server2.thrift.sasl.qop = auth-conf/g' ./config/hive_and_kerberos.cfg
     popd
 }
