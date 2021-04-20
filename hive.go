5dac1db5d0e81cd0f80a6f7b09bb1c7f90808cb2
Evan McIntire <evanm@dropbox.com>
2021-04-19 17:11:33 -0700

    Add channel to Cursor to populate logs during execution
    As a final followup to my previous PRs, add an optional cursor to the Cursor.
    
    If this channel is set, then during WaitForCompletion, and immediately after, logs will pushed into this channel, allowing an end user to retrieve Hive logs as a stream.

--- hive.go
+++ hive.go
@@ -380,6 +380,9 @@ type Cursor struct {
 	newData         bool
 	Err             error
 	description     [][]string
+
+	// Caller is responsible for managing this channel
+	Logs			chan<- []string
 }
 
 // WaitForCompletion waits for an async operation to finish
@@ -427,6 +430,15 @@ func (c *Cursor) WaitForCompletion(ctx context.Context) {
 		if c.Error() != nil {
 			return
 		}
+
+		if c.Logs != nil {
+			logs := c.FetchLogs()
+			if c.Error() != nil {
+				return
+			}
+			c.Logs <- logs
+		}
+
 		time.Sleep(time.Duration(time.Duration(c.conn.configuration.PollIntervalInMillis)) * time.Millisecond)
 		mux.Lock()
 		if contextDone {
@@ -466,6 +478,17 @@ func (c *Cursor) Execute(ctx context.Context, query string, async bool) {
 			}
 			return
 		}
+
+		// Flush logs after execution is finished
+		if c.Logs != nil {
+			logs := c.FetchLogs()
+			if c.Error() != nil {
+				c.state = _ASYNC_ENDED
+				return
+			}
+			c.Logs <- logs
+		}
+
 		c.state = _ASYNC_ENDED
 	}
 }
