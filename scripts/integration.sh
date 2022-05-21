5d4606ce6324138a200049a83e78d2fdcfce00ad
beltran <jaumemarhuenda@gmail.com>
2022-05-21 20:59:40 +0200

    fix tez version in hive_and_kerberos.cfg file

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -40,6 +40,7 @@ function install_deps() {
 
     python3.7.1 -m pip install --user -r requirements.txt
     sed -i.bak 's/tez_version.*/tez_version = 0.9.2/g' ./config/hive.cfg
+    sed -i.bak 's/tez_version.*/tez_version = 0.9.2/g' ./config/hive_and_kerberos.cfg
     sed -i.bak 's/hive_version.*/hive_version = 3.1.2/g' ./config/hive.cfg
     sed -i.bak 's/hive_version.*/hive_version = 3.1.2/g' ./config/hive_and_kerberos.cfg
     sed -i.bak 's/hadoop_version.*/hadoop_version = 2.10.1/g' ./config/hive.cfg
